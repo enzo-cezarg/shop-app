362fe785cb82f9673571264d6035986ee2dc9511
Enzo Garcia <enzoocezargarcia@gmail.com>
2025-05-12 16:49:49 -0300

    main page and styling done

---
+++ lib/components/product_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({
+    required this.product,
+    super.key,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          backgroundColor: Colors.black87,
+        ),
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shop/pages/products_overview_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: Colors.purple,
+          onPrimary: Colors.white,
+          secondary: Colors.deepOrange,
+          onSecondary: Colors.white,
+          error: Colors.red,
+          onError: Colors.white,
+          surface: Colors.white,
+          onSurface: Colors.black,
+        ),
+        fontFamily: 'Lato',
+        useMaterial3: true,
+      ),
+      home: ProductsOverviewPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
