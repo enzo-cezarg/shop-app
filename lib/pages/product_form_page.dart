fc174c86858e91d300634fa8decadf32c0245441
Enzo Garcia <enzoocezargarcia@gmail.com>
2025-05-30 16:58:44 -0300

    product form page

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class ProductFormPage extends StatelessWidget {
+class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
+
+  @override
+  State<ProductFormPage> createState() => _ProductFormPageState();
+}
+
+class _ProductFormPageState extends State<ProductFormPage> {
+  final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocus.dispose();
+    _descriptionFocus.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +25,36 @@ class ProductFormPage extends StatelessWidget {
         title: Text('New Product'),
         backgroundColor: Theme.of(context).colorScheme.primary,
         foregroundColor: Theme.of(context).colorScheme.onPrimary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Name'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_priceFocus),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                focusNode: _priceFocus,
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_descriptionFocus),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+                focusNode: _descriptionFocus,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
