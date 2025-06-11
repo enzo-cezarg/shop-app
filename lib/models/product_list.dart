bf4ecd673119503515aa63ef1bfc95ec89f06ed7
Enzo Garcia <enzoocezargarcia@gmail.com>
2025-06-11 17:17:33 -0300

    pull to refresh and update product

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -6,8 +6,8 @@ import 'package:http/http.dart' as http;
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
-  final _url =
-      'https://shop-app-flutter-b3be7-default-rtdb.firebaseio.com/products.json';
+  final _baseUrl =
+      'https://shop-app-flutter-b3be7-default-rtdb.firebaseio.com/products';
 
   //final List<Product> _items = dummyProducts;
   final List<Product> _items = [];
@@ -24,7 +24,9 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(
+      Uri.parse('$_baseUrl.json'),
+    );
     if (response.body == 'null') return;
 
     Map<String, dynamic> data = jsonDecode(response.body);
@@ -63,7 +65,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse('$_baseUrl.json'),
       body: jsonEncode({
         'name': product.name,
         'description': product.description,
@@ -88,15 +90,23 @@ class ProductList with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     int index = _items.indexWhere((p) => p.id == product.id);
 
     if (index >= 0) {
+      await http.patch(
+        Uri.parse('$_baseUrl/${product.id}.json'),
+        body: jsonEncode({
+          'name': product.name,
+          'description': product.description,
+          'price': product.price,
+          'imageUrl': product.imageUrl,
+        }),
+      );
+
       _items[index] = product;
       notifyListeners();
     }
-
-    return Future.value();
   }
 
   void removeProduct(Product product) {
